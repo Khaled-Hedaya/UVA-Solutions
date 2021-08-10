d4f8e6ab5ee39bfd5fa25cb002a58dae66c1d66a
Hedaya <khaledhedaya98@gmail.com>
2021-08-10 16:56:49 +0200

    Create 1124 Celebrity jeopardy.cpp

---
+++ Adhocs/Easy/1124 Celebrity jeopardy.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O3")
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_DISABLE_PERFCRIT_LOCKS
+#define FastIO() ios::sync_with_stdio(false), cin.tie(0);
+#define F first
+#define S second
+#define pb push_back
+#define ll long long
+#define All(v) v.begin() , v.end()
+#define rep(i,s,e) for (int i = s; i < e; ++i)
+#define repr(i,s,e) for (int i = s; i > e; --i)
+
+
+const int N = 1e5 + 10, OO = 0x3f3f3f3f;
+
+
+int main()
+{
+
+    string s;
+    while (getline(cin , s)) {
+        cout << s << "\n";
+    }
+
+
+    return 0;
+}
+
+
+
+
