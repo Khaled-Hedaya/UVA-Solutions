9facf2d5d9ce7d588566a705da797fe58c2b21ae
Hedaya <khaledhedaya98@gmail.com>
2021-10-06 16:09:56 +0200

    Create 280 Vertex.cpp

---
+++ Graphs/Traversals/280 Vertex.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#ifdef ONPC
+#define db(...) ({cout << "> Line " << __LINE__  \
+        << ": "; _db(#__VA_ARGS__, __VA_ARGS__);})
+#else
+#define db(...) true
+#endif
+
+template<class S, class T>
+ostream &operator<<(ostream &o, const map<S, T> &v) {
+    o << "[";
+    int i = 0;
+    for (const pair<S, T> &pr: v)
+        o << (!i++ ? "" : ", ") << "{"
+          << pr.fs << " : " << pr.sc << "}";
+    return o << "]";
+}
+
+template<template<class, class...> class S, class T, class... L>
+ostream &operator<<(ostream &o, const S<T, L...> &v) {
+    o << "[";
+    int i = 0;
+    for (const auto &e: v) o << (!i++ ? "" : ", ") << e;
+    return o << "]";
+}
+
+template<class S, class T>
+ostream &operator<<(ostream &o, const pair<S, T> &pr) {
+    return o << "(" << pr.fs << ", " << pr.sc << ")";
+}
+
+ostream &operator<<(ostream &o, const string &s) {
+    for (const char &c: s) o << c;
+    return o;
+}
+
+template<class T>
+void _db(const char *dbStr, T e) {
+    cout << dbStr << " = " << e << endl;
+}
+
+template<class T, class... L>
+void _db(const char *dbStr, T e, L... r) {
+    while (*dbStr != ',') cout << *dbStr++;
+    cout << " = " << e << ',';
+    _db(dbStr + 1, r...);
+}
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define FastIO() ios::sync_with_stdio(false), cin.tie(0)
+#define F first
+#define S second
+#define pb push_back
+#define ll long long
+#define SZ(a) ((int)((a).size()))
+#define All(v) v.begin(), v.end()
+#define rep(i, s, e) for (int i = s; i < e; ++i)
+#define repr(i, s, e) for (int i = s; i > e; --i)
+
+const int N = 1e5 + 10, OO = 0x3f3f3f3f;
+
+/*
+"Maybe they aren't stars.. Maybe they are a map to get out of here"
+*/
+
+bool vis[105];
+vector<vector<int>> adj;
+
+void dfs(int src, bool frst) {
+    if (!frst)
+        vis[src] = 1;
+    for (auto ch: adj[src])
+        if (!vis[ch])
+            dfs(ch, 0);
+}
+
+void solve() {
+    int n;
+    while (cin >> n, n) {
+        memset(vis, 0, sizeof vis);
+        adj = vector<vector<int>>(n + 1);
+        int x;
+        while (1) {
+            cin >> x;
+            if (!x) break;
+            int y;
+            while (cin >> y, y)
+                adj[x].push_back(y);
+        }
+        cin >> x;
+        for (int i = 0; i < x; ++i) {
+            int s;
+            cin >> s;
+            memset(vis, 0, sizeof vis);
+            dfs(s, 1);
+            vector<int> ans;
+            for (int j = 1; j <= n; ++j) {
+                if (!vis[j])
+                    ans.push_back(j);
+            }
+            int sz = SZ(ans);
+            cout << sz;
+            for (int j = 0; j < sz; ++j) {
+                cout << " " << ans[j];
+            }
+            cout << "\n";
+        }
+    }
+
+}
+
+int main() {
+
+    FastIO();
+#ifdef ONPC
+    freopen("in.txt", "r", stdin);
+#endif
+
+    int tc = 1;
+    // cin >> tc;
+    for (int i = 1; i <= tc; i++) {
+        solve();
+#ifdef ONPC
+        cout << "__________________________" << "\n";
+#endif
+    }
+
+    return 0;
+}
