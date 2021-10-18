934215e708aae74e7d3968ed72e8dbcb5b45f540
Hedaya <khaledhedaya98@gmail.com>
2021-10-18 22:51:23 +0200

    Create 11561 Getting Gold.cpp

---
+++ Graphs/11561 Getting Gold.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#ifdef ONPC
+#define db(...) ({cout << "> Line " << __LINE__  \
+        << ": "; _db(#__VA_ARGS__, __VA_ARGS__);})
+#else
+#define db(...) true
+#endif
+
+template<class S, class T>
+ostream &operator<<(ostream &o, const map<S, T> &v) {
+    o << "[";
+    int i = 0;
+    for (const pair<S, T> &pr: v)
+        o << (!i++ ? "" : ", ") << "{"
+          << pr.fs << " : " << pr.sc << "}";
+    return o << "]";
+}
+
+template<template<class, class...> class S, class T, class... L>
+ostream &operator<<(ostream &o, const S<T, L...> &v) {
+    o << "[";
+    int i = 0;
+    for (const auto &e: v) o << (!i++ ? "" : ", ") << e;
+    return o << "]";
+}
+
+template<class S, class T>
+ostream &operator<<(ostream &o, const pair<S, T> &pr) {
+    return o << "(" << pr.fs << ", " << pr.sc << ")";
+}
+
+ostream &operator<<(ostream &o, const string &s) {
+    for (const char &c: s) o << c;
+    return o;
+}
+
+template<class T>
+void _db(const char *dbStr, T e) {
+    cout << dbStr << " = " << e << endl;
+}
+
+template<class T, class... L>
+void _db(const char *dbStr, T e, L... r) {
+    while (*dbStr != ',') cout << *dbStr++;
+    cout << " = " << e << ',';
+    _db(dbStr + 1, r...);
+}
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define FastIO() ios::sync_with_stdio(false), cin.tie(0)
+#define F first
+#define S second
+#define pb push_back
+#define ll long long
+#define SZ(a) ((int)((a).size()))
+#define All(v) v.begin(), v.end()
+#define rep(i, s, e) for (int i = s; i < e; ++i)
+#define repr(i, s, e) for (int i = s; i > e; --i)
+
+const int N = 1e5 + 10, OO = 0x3f3f3f3f;
+
+/*
+"Maybe they aren't stars.. Maybe they are a map to get out of here"
+*/
+const int dx[] = {0, 0, -1, 1};
+const int dy[] = {1, -1, 0, 0};
+int n, m, sr, sc, vis[55][55], cnt;
+char grid[55][55];
+
+void dfs(int r, int c) {
+    if (grid[r][c] == 'G')
+        cnt++;
+    vis[r][c] = 1;
+    for (int d = 0; d < 4; ++d) {
+        int nr = r + dx[d];
+        int nc = c + dy[d];
+        if (grid[nr][nc] == 'T')
+            return;
+    }
+    for (int d = 0; d < 4; ++d) {
+        int nr = r + dx[d];
+        int nc = c + dy[d];
+        if (!vis[nr][nc] && (grid[nr][nc] == '.' || grid[nr][nc] == 'G'))
+            dfs(nr, nc);
+    }
+}
+
+
+void solve() {
+    cnt = 0, sr = -1;
+    memset(vis, 0, n * sizeof vis[0]);
+    for (int i = 0; i < n; ++i) {
+        scanf("%s", grid[i]);
+    }
+    for (int i = 0; i < n && sr == -1; ++i) {
+        for (int j = 0; j < m && sr == -1; ++j) {
+            if (grid[i][j] == 'P')
+                sr = i, sc = j;
+        }
+    }
+    dfs(sr, sc);
+    printf("%d\n", cnt);
+
+}
+
+int main() {
+
+//    FastIO();
+#ifdef ONPC
+    freopen("in.txt", "r", stdin);
+#endif
+
+
+    // cin >> tc;
+    while (~scanf("%d %d", &m, &n)) {
+        solve();
+#ifdef ONPC
+        puts("-------------");
+#endif
+    }
+
+    return 0;
+}
